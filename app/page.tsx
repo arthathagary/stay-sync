cf3067b34278b12bfa59100f7e22f3d93d667ef2
Arthath Agary <arthathagary@gmail.com>
2023-12-03 15:33:05 +0530

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import { Button } from "@/components/ui/button"
+import { ModeToggle } from '@/components/toggle-mode'
+
+export default function Home() {
+  return (
+        <div>
+          <Button>Click me</Button>
+          <ModeToggle />
+        </div>
+      
+   
+    
+  )
+}
