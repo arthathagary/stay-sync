15db0f90d27c3835d8a7ab96f081aa783899cd08
Arthath Agary <arthathagary@gmail.com>
2023-12-05 18:20:19 +0530

    NavBar ui done

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import { Button } from "@/components/ui/button"
 import { ModeToggle } from '@/components/toggle-mode'
+import NavBar from '@/components/NavBar'
 
 export default function Home() {
   return (
         <div>
-          <Button>Click me</Button>
-          <ModeToggle />
+          {/* <Button>Click me</Button>
+          <ModeToggle /> */}
+          <NavBar />
         </div>
       
    
